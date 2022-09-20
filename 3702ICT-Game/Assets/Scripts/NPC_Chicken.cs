bb1fdf6ad5587b78dfb195d098b862cf4fa40cca
L0adingGit <elliottahorne@gmail.com>
2022-09-20 12:43:46 +1000

    Merged NPC + Race Mechanic Branches
    Used an exported package to merch NPC + Race Mechanic bracnhes. Cleaned up folders a little bit as well

---
+++ 3702ICT-Game/Assets/Scripts/NPC_Chicken.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NPC_Chicken : MonoBehaviour
+{
+
+    public GameObject[] waypointList;
+    private NavMeshAgent nav;
+    public int patrolCount = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        nav = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        nav.SetDestination(waypointList[patrolCount].transform.position);
+        float dist = Vector3.Distance(transform.position, waypointList[patrolCount].transform.position);
+       
+       
+
+        if (dist <= 2.3)//<- number might change when asset is applied
+        {
+            
+            patrolCount = patrolCount + 1;
+            
+            if (patrolCount == 2 && dist <= 2.3)
+            {
+                patrolCount = 0;
+            }
+        }
+
+    }
+
+ 
+}
---
+++ 3702ICT-Game/Assets/Week04/Scripts/PlayerTank.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class PlayerTank : MonoBehaviour
+{
+
+	public float moveSpeed = 10.0f;  // units per second
+	public float rotateSpeed = 20.0f;
+	public float playerHealth = 100;
+
+	public Transform PlayerTransform;
+	public Transform UFO_Waypoint;
+
+	public TMP_Text healthText;
+
+
+	private Transform _transform;
+	private Rigidbody _rigidbody;
+
+
+	// Use this for initialization
+	void Start()
+	{
+		_transform = transform;
+		_rigidbody = GetComponent<Rigidbody>();
+		rotateSpeed = rotateSpeed * 180 / Mathf.PI; // convert from rad to deg for rot function
+
+		PlayerTransform = GameObject.Find("Player").transform;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+		// check for input
+		float rot = _transform.localEulerAngles.y + rotateSpeed * Time.deltaTime * Input.GetAxis("Horizontal");
+		Vector3 fwd = _transform.forward * moveSpeed * Time.deltaTime * Input.GetAxis("Vertical");
+
+		// Tank Chassis is rigidbody, use MoveRotation and MovePosition
+		GetComponent<Rigidbody>().MoveRotation(Quaternion.AngleAxis(rot, Vector3.up));
+		GetComponent<Rigidbody>().MovePosition(_rigidbody.position + fwd);
+
+		healthText.text = "Health: " + playerHealth;
+
+		if(playerHealth == 0)
+        {
+			SceneManager.LoadScene("NPC Testing");
+        }
+	}
+
+	public void RestartGame() {
+		SceneManager.LoadScene("Week04");
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Slowdown")
+        {
+			moveSpeed = moveSpeed / 2;
+        }
+		
+		if(other.tag == "Chicken")
+        {
+			playerHealth = playerHealth - 25;
+        }
+
+		if (other.tag == "UFO")
+        {
+			PlayerTransform.position = UFO_Waypoint.position;
+
+		}
+    }
+    private void OnTriggerExit(Collider other)
+    {
+		if (other.tag == "Slowdown")
+		{
+			moveSpeed = 10.0f;
+		}
+	}
+
+}
