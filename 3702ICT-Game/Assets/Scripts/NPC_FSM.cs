299048f1b563ea1b87fda599bed84157640d650c
Toshimitsu Ota <89127228+toshimitsu-o@users.noreply.github.com>
2022-10-09 10:26:54 +1000

    Racer get destroyed when reaches goal

--- 3702ICT-Game/Assets/Scripts/NPC_FSM.cs
+++ 3702ICT-Game/Assets/Scripts/NPC_FSM.cs
@@ -22,6 +22,7 @@ public class NPC_FSM : MonoBehaviour
     public GameObject[] waypointList;
 
     public TMP_Text enemyStatusText; // Text for enemy status UI
+    private bool isFinished;
 
 
     // Start is called before the first frame update
@@ -33,40 +34,48 @@ public class NPC_FSM : MonoBehaviour
         currentWaypoint = 0;
         // Status text to set empty
         enemyStatusText.text = "0";
+        isFinished = false;
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
         switch (curState)
         {
             case FSMState.Race_Start: UpdateRaceStartState(); break;
-            //case FSMState.Race_Finish: UpdateRaceFinishState(); break;
+            case FSMState.Race_Finish: UpdateRaceFinishState(); break;
         }
-
     }
 
     protected void UpdateRaceStartState()
     {
         nav.SetDestination(waypointList[currentWaypoint].transform.position);
-
+        
         // When NPC get to the current waypoint
         if (Vector3.Distance(transform.position, waypointList[currentWaypoint].transform.position) < 0.5)
         {
             if (currentWaypoint >= waypointList.Length -1)
-            {
-                Debug.Log("Goal!");
+            { // Reached to the final destination
+                //Debug.Log("Goal!");
+                // UI text update
                 enemyStatusText.text = "Finished!";
+                // Hide itself
+                //gameObject.SetActive(false);
+                Destroy(gameObject);
+                curState = FSMState.Race_Finish;
             } else
             {
+                // Set next in array for waypoint
                 currentWaypoint += 1;
+                // UI text update
                 enemyStatusText.text = currentWaypoint + " collected";
             }
-            nav.SetDestination(waypointList[currentWaypoint].transform.position);
+            //nav.SetDestination(waypointList[currentWaypoint].transform.position);
         }
     }
+    protected void UpdateRaceFinishState()
+    {
 
-   
+    }
 }
